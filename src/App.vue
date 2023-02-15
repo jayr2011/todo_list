36170b9043ea62336554d611a5900323405b25ff
Jair Costa <juniorceia1996@gmail.com>
2023-02-15 20:20:15 -0300

    implementação de lista de tarefas e a possibilidade de poder apaga-la

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ const inputCategoryRadio = ref(null);
 
 const todosAsync = computed(() =>
   todos.value.sort((a, b) => {
-    return b.createdAt - a.createdAt;
+    return a.createdAt - b.createdAt;
   })
 );
 
@@ -20,11 +20,17 @@ const addTodo = () => {
   } else {
     todos.value.push({
       content: inputContentText.value,
-      category: inputCategoryRadio,
+      category: inputCategoryRadio.value,
       done: false,
       createdAt: new Date().getTime(),
     });
+    inputContentText.value = "";
+    inputCategoryRadio.value = null;
   }
+};
+
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
 };
 
 watch(
@@ -41,26 +47,27 @@ watch(name, (newVal) => {
 
 onMounted(() => {
   name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
 });
 </script>
 <template>
   <main class="app">
     <section class="greeting">
       <h2 class="title">
-        Whats up,
-        <input type="text" placeholder="name here" v-model="name" />
+        como vai
+        <input type="text" placeholder="Seu nome aqui" v-model="name" />
       </h2>
     </section>
     <section class="create-todo">
-      <h3>create a todo</h3>
+      <h3>Crie suas Tarefas</h3>
       <form @submit.prevent="addTodo">
-        <h4>Whats on your todo list ?</h4>
+        <h4>O que você quer adicionar a sua lista de tarefas ?</h4>
         <input
           type="text"
-          placeholder="e.g make a video"
+          placeholder="Crie uma tarefa"
           v-model="inputContentText"
         />
-        <h4>pick a category</h4>
+        <h4>Por favor, Escolha uma categoria</h4>
         <div class="options">
           <label>
             <input
@@ -70,7 +77,7 @@ onMounted(() => {
               v-model="inputCategoryRadio"
             />
             <span class="bubble business"></span>
-            <div>Business</div>
+            <div>Trabalho</div>
           </label>
           <label>
             <input
@@ -80,11 +87,40 @@ onMounted(() => {
               v-model="inputCategoryRadio"
             />
             <span class="bubble personal"></span>
-            <div>Personal</div>
+            <div>Pessoal</div>
           </label>
         </div>
-        <input type="submit" value="Add todo" />
+        <input type="submit" value="Adicionar Tarefa" />
       </form>
     </section>
+    <section v-if="todos.length > 0" class="todo-list">
+      <h3>Tarefas</h3>
+      <div class="list">
+        <div
+          v-for="todo in todosAsync"
+          :class="`todo-item ${todo.done && 'done'}`"
+        >
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span :class="`bubble ${todo.category}`"></span>
+          </label>
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Deletar</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div v-else class="todo-list__empty">
+      <span> Que pena, você ainda não tem tarefas :( </span>
+    </div>
   </main>
 </template>
+
+<style>
+.todo-list__empty {
+  text-align: center;
+}
+</style>
